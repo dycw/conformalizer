0ba63c0ae0bce02748fd6e176a5e94f6dcdb7ad4
Derek Wan <d.wan@icloud.com>
2025-12-14 09:30:14 +0900

    2025-12-14 09:30:14 (Sun)  > DW-Mac  > derekwan

--- script.py
+++ script.py
@@ -11,18 +11,23 @@
 # ///
 from __future__ import annotations
 
+from contextlib import contextmanager
 from logging import getLogger
 from pathlib import Path
 from typing import TYPE_CHECKING
 
 from click import command
-from tomlkit import dumps, parse
-from tomlkit.items import Table
+from tomlkit import array, dumps, parse, table
+from tomlkit.items import Array, Table
 from typed_settings import click_options, settings
 from utilities.click import CONTEXT_SETTINGS_HELP_OPTION_NAMES
+from utilities.functions import ensure_class
 from utilities.logging import basic_config
 
 if TYPE_CHECKING:
+    from collections.abc import Iterator
+
+    from tomlkit.container import Container
     from utilities.types import PathLike
 
 _LOGGER = getLogger(__name__)
@@ -30,7 +35,8 @@ _LOGGER = getLogger(__name__)
 
 @settings()
 class Settings:
-    pyproject_build_system: bool = False
+    pyproject__build_system: bool = False
+    pyproject__dependency_groups: bool = False
     dry_run: bool = False
 
 
@@ -41,23 +47,10 @@ def main(settings: Settings, /) -> None:
         _LOGGER.info("Dry run; exiting...")
         return
     _LOGGER.info("Running...")
-    if settings.pyproject_build_system:
+    if settings.pyproject__build_system:
         _add_pyproject_build_system()
-
-
-def _add_pyproject_build_system(*, path: PathLike = "pyproject.toml") -> None:
-    path = Path(path)
-    _add_pyproject(path=path)
-    existing = parse(path.read_text())
-    new = existing.copy()
-    new.setdefault("build-system", {})
-    if not isinstance(build_system := new["build-system"], Table):
-        raise TypeError(build_system, type(build_system))
-    build_system["build-backend"] = "uv_build"
-    build_system["requires"] = ["uv_build"]
-    if new != existing:
-        _LOGGER.info("Adding `pyproject.toml` [build-system]...")
-        _ = path.write_text(dumps(new))
+    if settings.pyproject__dependency_groups:
+        _add_pyproject_dependency_groups()
 
 
 def _add_pyproject(*, path: PathLike = "pyproject.toml") -> None:
@@ -65,6 +58,37 @@ def _add_pyproject(*, path: PathLike = "pyproject.toml") -> None:
     if not path.is_file():
         _LOGGER.info("Adding `%s`...", path)
         path.touch()
+
+
+def _add_pyproject_build_system(*, path: PathLike = "pyproject.toml") -> None:
+    with _yield_pyproject("[build-system]", path=path) as doc:
+        bs = ensure_class(doc.setdefault("build-system", table()), Table)
+        bs["build-backend"] = "uv_build"
+        bs["requires"] = ["uv_build"]
+
+
+def _add_pyproject_dependency_groups(*, path: PathLike = "pyproject2.toml") -> None:
+    with _yield_pyproject("[dependency-groups]", path=path) as doc:
+        db = ensure_class(doc.setdefault("dependency-groups", table()), Table)
+        dev = ensure_class(db.setdefault("dev", array()), Array)
+        if (dycw := "dycw-utilities[test]") not in dev:
+            dev.append(dycw)
+        if (rich := "rich") not in dev:
+            dev.append(rich)
+
+
+@contextmanager
+def _yield_pyproject(
+    desc: str, /, *, path: PathLike = "pyproject.toml"
+) -> Iterator[Container]:
+    path = Path(path)
+    _add_pyproject(path=path)
+    temp = parse(path.read_text())
+    yield temp
+    current = parse(path.read_text())
+    if current != temp:
+        _LOGGER.info("Adding `pyproject.toml` %s...", desc)
+        _ = path.write_text(dumps(temp))
 
 
 if __name__ == "__main__":
