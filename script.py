90d760663c5c4ad77816de510e9ab3d5083b7579
Derek Wan <d.wan@icloud.com>
2025-12-24 08:56:57 +0900

    Use `--github--pull-request--pyright` (#92)

--- script.py
+++ script.py
@@ -4,12 +4,13 @@
 # dependencies = [
 #   "click >= 8.3.1, < 8.4",
 #   "dycw-utilities >= 0.172.6, < 0.173",
-#   "pytest-xdist >= 3.8.0, < 3.9",
 #   "pyyaml >= 6.0.3, < 6.1",
 #   "rich >= 14.2.0, < 14.3",
 #   "tomlkit >= 0.13.3, < 0.14",
 #   "typed-settings[attrs, click] >= 25.3.0, < 25.4",
 #   "xdg-base-dirs >= 6.0.2, < 6.1",
+#   "pyright",
+#   "pytest-xdist",
 # ]
 # ///
 from __future__ import annotations
