a12f26cc2dc1547c372ceb7548a76041664f011d
Derek Wan <d.wan@icloud.com>
2026-01-01 11:05:07 +0900

    2026-01-01 11:05:07 (Thu)  > DW-Mac  > derekwan

--- script.py
+++ script.py
@@ -172,10 +172,11 @@ class Settings:
     python_package_name: str | None = option(
         default=None, help="Python package name override"
     )
-    python_version: str = option(default="3.14", help="Python version")
+    # python_version: str = option(default="3.14", help="Python version")
+    python_version: str = option(default="3.12", help="Python version")
     readme: bool = option(default=False, help="Set up 'README.md'")
     repo_name: str | None = option(default=None, help="Repo name")
-    ruff: bool = option(default=False, help="Set up 'ruff.toml'")
+    ruff: bool = option(default=True, help="Set up 'ruff.toml'")
     script: str | None = option(
         default=None, help="Set up a script instead of a package"
     )
