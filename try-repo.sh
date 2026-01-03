e0e1f109e0ef93730d9cca92960d4162d6b63758
Derek Wan <d.wan@icloud.com>
2026-01-03 15:23:21 +0900

    2026-01-03 15:23:21 (Sat)  > DW-Mac  > derekwan

--- try-repo.sh
+++ try-repo.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
-# add hook to `project.scripts` and `.pre-commit-hooks.yaml`
-# edit `HOOK_NAME` below
-# in your project, run:
-#     ~/personal/conformalize/try-repo.sh
-
 PATH_DIR="$(
 	cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit
 	pwd -P
 )"
-
 pre-commit try-repo --verbose --all-files "${PATH_DIR}" conformalize "$@"
